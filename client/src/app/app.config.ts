f0e587b4b2fc38ed87aa4d583aa12e330fd2c51c
YelovSK <hampatrik@gmail.com>
2026-05-01 00:26:58 +0200

    add PWA support

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,6 +6,8 @@ import { routes } from './app.routes';
 import { errorInterceptor } from '@services/api/error.interceptor';
 import { authInterceptor } from '@services/api/auth.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
+import { environment } from '@env/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ]
 };
